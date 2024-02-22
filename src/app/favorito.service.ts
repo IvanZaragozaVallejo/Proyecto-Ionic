3de1b4f3acc75678a6b24b8e45cc3bb4e5e3d777
owy <8824483@alu.murciaeduca.es>
2024-02-22 17:15:58 +0100

    Revert "cambios en algunas configuraciones"
    This reverts commit 06d9f8086d5cfab231f509fd9ab4afc98606e1b0.

--- src/app/favorito.service.ts
+++ src/app/favorito.service.ts
@@ -3,38 +3,39 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class FavoriteService {
-  private favorites: Set<string> = new Set();
+export class FavoritoService {
+  private favoritos: string[] = [];
 
   constructor() {}
 
-  addFavorite(city: string): void {
-    if (!this.favorites.has(city)) {
-      this.favorites.add(city);
-      this.saveFavorites();
+  agregarFavorito(ciudad: string) {
+    if (!this.favoritos.includes(ciudad)) {
+      this.favoritos.push(ciudad);
+    }
+    this.guardarFavoritos();
+  }
+
+  obtenerFavoritos(): string[] {
+    this.cargarFavoritos();
+    return this.favoritos;
+  }
+
+  eliminarFavorito(ciudad: string) {
+    const index = this.favoritos.indexOf(ciudad);
+    if (index !== -1) {
+      this.favoritos.splice(index, 1);
+      this.guardarFavoritos();
     }
   }
 
-  getFavorites(): string[] {
-    this.loadFavorites();
-    return Array.from(this.favorites);
-  }
-
-  removeFavorite(city: string): void {
-    if (this.favorites.has(city)) {
-      this.favorites.delete(city);
-      this.saveFavorites();
+  private cargarFavoritos() {
+    const storedFavoritos = localStorage.getItem('favoritos');
+    if (storedFavoritos) {
+      this.favoritos = JSON.parse(storedFavoritos);
     }
   }
 
-  private loadFavorites(): void {
-    const storedFavorites = localStorage.getItem('favorites');
-    if (storedFavorites) {
-      this.favorites = new Set(JSON.parse(storedFavorites));
-    }
-  }
-
-  private saveFavorites(): void {
-    localStorage.setItem('favorites', JSON.stringify(Array.from(this.favorites)));
+  private guardarFavoritos() {
+    localStorage.setItem('favoritos', JSON.stringify(this.favoritos));
   }
 }
